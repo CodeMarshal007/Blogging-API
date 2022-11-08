2ebd4042a6cc42afbf125b5fc42e3ea406ccfc09
ABDULMAJEED ADENIYI YUSUF <102981590+CodeMarshal007@users.noreply.github.com>
2022-11-08 15:17:55 +0100

    Update connctToDb.js

--- connectToDB/connctToDb.js
+++ connectToDB/connctToDb.js
@@ -1,25 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const {MONGODB_URI} = require("../config.js)
 
-const ATLAS_URL = process.env.ATLAS_URL;
+
 // Function that handles connection to database
 function connectToDatabase() {
-  mongoose.connect(ATLAS_URL);
-
-  mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB Atlas Successfully");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.log("An error occurred while connecting to MongoDB");
-    console.log(err);
-  });
-}
-
-// connction to test db
-const URL = process.env.URL;
-function connectToTestDatabase() {
-  mongoose.connect(URL);
+  mongoose.connect(MONGODB_URI);
 
   mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB Successfully");
@@ -31,4 +17,6 @@ function connectToTestDatabase() {
   });
 }
 
-module.exports = { connectToDatabase, connectToTestDatabase };
+
+
+module.exports = { connectToDatabase};
