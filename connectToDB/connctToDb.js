115c27a9ead4b1da202ffbf60f131d023d598b07
Adeniyi A. Yusuf <princeaayusuf@gmail.com>
2022-11-08 21:57:33 +0100

    resolve database connection error

--- connectToDB/connctToDb.js
+++ connectToDB/connctToDb.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const { MONGODB_URI } = require("../config");
 
-const ATLAS_URL = process.env.ATLAS_URL;
 // Function that handles connection to database
 function connectToDatabase() {
-  mongoose.connect(ATLAS_URL);
+  mongoose.connect(MONGODB_URI);
 
   mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB Atlas Successfully");
@@ -16,19 +16,4 @@ function connectToDatabase() {
   });
 }
 
-// connction to test db
-const URL = process.env.URL;
-function connectToTestDatabase() {
-  mongoose.connect(URL);
-
-  mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB Successfully");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.log("An error occurred while connecting to MongoDB");
-    console.log(err);
-  });
-}
-
-module.exports = { connectToDatabase, connectToTestDatabase };
+module.exports = { connectToDatabase };
